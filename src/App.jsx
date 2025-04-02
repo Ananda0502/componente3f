e47db3c22927fb47623090cc7212176124f05fb7
Ananda0502 <marciaevelly7@gmail.com>
2025-04-02 10:19:09 -0300

    componetes

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Botao from "./componentes/Botao"
+import Caixa from "./componentes/Caixa"
+import Etiqueta from "./componentes/Etiqueta"
+
+ const App = ()=> {
+   return (
+    <>
+    <h3>Aula Retomada Componentes</h3>
+    <Etiqueta texto= "Texto enviado pelo App"/>
+    <Etiqueta texto= "Não ligo"/>
+    <Etiqueta />
+    <Caixa />
+    <Botao />
+    </>
+   )
+ }
+
+ export default App
+
+
+
+
+
+
+
+
+
